76113f2e9299bebf0a6c9986c68687ff576142a7
Inn-purity <innkeshry@gmail.com>
2024-05-09 16:39:04 +0300

    this is my first commit

---
+++ java.js
@@ -0,0 +1,23 @@
+function welcomeAndCalculate()
+{alert("welcome to our website!");
+var a=5;
+var b=20;
+var c="jet";
+var d="leclerc";
+var sum=a +b;
+var quotient= a/b;
+document.getElementById("sumDisplay").innerHTML="Sum a+b=" +sum;
+document.getElementById("display").innerHTML="Quotient a/b=" +quotient;
+document.getElementById("display2").innerHTML= c +" " +d;
+var a="5";
+var b="20";
+document.getElementById("display3").innerHTML= c +" " +d +" " +b+a;
+document.getElementById("display4").innerHTML= sum +" " +c +" " +d;
+}
+console.log(a<b);
+console.log(a<c);
+console.log(d<a);
+console.log(b<c);
+console.log(b<d);
+
+
